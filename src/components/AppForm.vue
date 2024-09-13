60b9bc8b9aeb9d609172b94ca3539db6c3044396
abduraufaken <abduraufaze@gmail.com>
2024-09-13 16:26:54 +0500

    update

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="form-wrapper">
     <div class="feedback-form" data-aos="fade-top">
-      <h2>{{t('form.form__title')}}</h2>
+      <h2>{{ t('form.form__title') }}</h2>
       <form @submit.prevent="submitForm">
-        <input type="text" v-model="name" :placeholder="t('form.name__place')" required />
-        <input type="tel" v-model="phone" :placeholder="t('form.phone__place')" required />
+        <input
+          type="text"
+          v-model="name"
+          :placeholder="t('form.name__place')"
+          required
+        />
+        <input
+          type="tel"
+          v-model="phone"
+          :placeholder="t('form.phone__place')"
+          required
+        />
         <textarea v-model="message" :placeholder="t('form.text__place')"></textarea>
-        <button type="submit">{{t('form.submit')}}</button>
+        <button type="submit">{{ t('form.submit') }}</button>
       </form>
     </div>
   </div>
 </template>
 
 <script>
-import { onMounted,ref,  inject } from 'vue';
-import AOS from 'aos';
+import { onMounted, ref, inject } from "vue";
+import AOS from "aos";
+import Swal from "sweetalert2";
 
 export default {
   setup() {
@@ -26,28 +37,41 @@ export default {
 
     const submitForm = () => {
       if (name.value && phone.value && message.value) {
-        alert(`Ism: ${name.value}\nTelefon: ${phone.value}\nXabar: ${message.value}`);
-        // formani reset qilish
+        Swal.fire({
+          title: "Muvaffaqiyatli bajarildi!",
+          html: `
+            <strong>Ism:</strong> ${name.value} <br>
+            <strong>Telefon:</strong> ${phone.value} <br>
+            <strong>Xabar:</strong> ${message.value}
+          `,
+          icon: "success",
+          confirmButtonText: "OK",
+        });
         name.value = "";
         phone.value = "";
         message.value = "";
       } else {
-        alert("Barcha maydonlarni to'ldiring.");
+        Swal.fire({
+          title: "Xatolik!",
+          text: "Barcha maydonlarni to'ldiring.",
+          icon: "error",
+          confirmButtonText: "OK",
+        });
       }
     };
 
-
     onMounted(() => {
       AOS.init({
-        duration: 1000, // Animatsiya davomiyligi
+        duration: 1000,
       });
     });
+
     return {
       name,
       phone,
       message,
       submitForm,
-      t
+      t,
     };
   },
 };
